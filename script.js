7d40dfdb260afcfe7ba2fafe5f38424842f48336
Sharmin16103284 <sharmin.16103284@gmail.com>
2022-04-11 01:30:24 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// second slider js code
+
+$(document).ready(function(){
+    $('.client-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
